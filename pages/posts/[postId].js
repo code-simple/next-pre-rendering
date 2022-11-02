4183049aaeea75d1679c8d4382dca802a087da15
Code Simple <zabardast@gmail.com>
2022-11-03 01:23:12 +0500

    changes

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,54 +1,41 @@
-export default function Post({post}) {
+export default function Post({ post }) {
     return (
         <>
-        <h2>
-            {post.id} {post.title}
-        </h2>
-        <p>{post.body}</p>
+            <h2>
+                {post.id} {post.title}
+            </h2>
+            <p>{post.body}</p>
         </>
     )
 }
 
 // This is the only way to handle hundreds of posts if there are
-export async function getStaticPaths(){
-    const paths = [];
-    for(let i = 0; i<105; i++){
-        let paramObj = {params: ""};
-        paramObj.params = {postId: `${i}`}
-        paths.push(paramObj);
-    }
-    console.log(paths)
+export async function getStaticPaths() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    const paths = data.map(post => {
+        return {
+            params: {
+                postId: `${post.id}`
+            }
+        }
+    })
     return {
-        paths: paths,
+        paths,
         fallback: false
     }
 }
-// export async function getStaticPaths(){
-//  return {
-//     paths:[
-//         {
-//             params:{postId:'1'}
-//         },
-//         {
-//             params:{postId:'2'}
-//         },
-//         {
-//             params:{postId:'3'}
-//         },
-//     ],
-//     fallback: false
-//  }
-// }
+
 
 
 export async function getStaticProps(context) {
     const { params } = context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await response.json()
-    
+
     return {
-        props:{
-            post:data,
+        props: {
+            post: data,
         }
     }
 }
